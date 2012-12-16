6ddddd0e0995e5ad15ae4d410bac6a7448285f84
Leigh Pauls <leigh.pauls@gmail.com>
2012-12-16 00:22:32 -0800

    Add an asynchronous conflicting test

--- test.py
+++ test.py
@@ -29,17 +29,31 @@ def simple_test():
 
     print s
 
-def make_change_sender(other_remote):
-    def _change_sender(change):
-        print "Change being sent:", change
-        other_remote.handle_remote_change(change)
-    return _change_sender
+class AsyncBuffer:
+    def __init__(self):
+        self.events = []
 
-def make_ack_sender(other_remote):
-    def _ack_sender():
-        print "Ack being sent"
-        other_remote.handle_remote_ack()
-    return _ack_sender
+    def make_change_sender(self, other_remote):
+        def _change_sender(change):
+            print "Change being sent:", change
+            def _send_change():
+                print "Change being executed:", change
+                other_remote.handle_remote_change(change)
+            self.events.append(_send_change)
+        return _change_sender
+
+    def make_ack_sender(self, other_remote):
+        def _ack_sender():
+            print "Ack being sent"
+            def _send_ack():
+                other_remote.handle_remote_ack()
+            self.events.append(_send_ack)
+        return _ack_sender
+
+    def resolve_events(self):
+        for event in self.events:
+            event()
+        events = []
 
 def unconcurrent_test():
     # make the main server first, and give it some value
@@ -59,18 +73,27 @@ def unconcurrent_test():
     ext_server = jup.Server(ext_to_main_remote, ext_initer)
 
     # tie the remotes together
-    ext_to_main_remote.set_local_change_cb(make_change_sender(main_to_ext_remote))
-    
-    ext_to_main_remote.set_ack_remote_change_cb(make_ack_sender(main_to_ext_remote))
+    buf = AsyncBuffer()
 
-    main_to_ext_remote.set_local_change_cb(make_change_sender(ext_to_main_remote))
-    main_to_ext_remote.set_ack_remote_change_cb(make_ack_sender(ext_to_main_remote))
+    ext_to_main_remote.set_local_change_cb(
+        buf.make_change_sender(main_to_ext_remote))
+    ext_to_main_remote.set_ack_remote_change_cb(
+        buf.make_ack_sender(main_to_ext_remote))
+
+    main_to_ext_remote.set_local_change_cb(
+        buf.make_change_sender(ext_to_main_remote))
+    main_to_ext_remote.set_ack_remote_change_cb(
+        buf.make_ack_sender(ext_to_main_remote))
 
     # send some changes
+    ext_server.apply_local_change(
+        jup.Operation(jup.OP_INSERT, 1, "d", ext_server.precedence))
     main_server.apply_local_change(
         jup.Operation(jup.OP_INSERT, 0, "c", main_server.precedence)) 
     main_server.apply_local_change(
         jup.Operation(jup.OP_INSERT, 2, "b", main_server.precedence))
+
+    buf.resolve_events()
 
     print main_server.value
     print ext_server.value
